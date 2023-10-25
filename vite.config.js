fd2dc72b37e69fa63a03729af760395e72407c7a
mbotela <leemaliy53@gmail.com>
2023-10-25 17:42:40 +0300

    changed vite config file syntax

--- vite.config.js
+++ vite.config.js
@@ -7,4 +7,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  resolve: {
+    alias: {
+      '@mui/x-charts': '@mui/x-charts', 
+    }
+  }
 })
