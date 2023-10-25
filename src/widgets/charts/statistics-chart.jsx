a368f79c955893943a1cffd78d26b44543c749b2
mbotela <leemaliy53@gmail.com>
2023-10-25 17:44:05 +0300

    removed chart syntax

--- src/widgets/charts/statistics-chart.jsx
+++ src/widgets/charts/statistics-chart.jsx
@@ -1,60 +1,60 @@
-import {
-  Card,
-  CardHeader,
-  CardBody,
-  CardFooter,
-  Typography,
-} from "@material-tailwind/react";
-import PropTypes from "prop-types";
-import Chart from "react-apexcharts";
+// import {
+//   Card,
+//   CardHeader,
+//   CardBody,
+//   CardFooter,
+//   Typography,
+// } from "@material-tailwind/react";
+// import PropTypes from "prop-types";
+// import Chart from "react-apexcharts";
 
-export function StatisticsChart({ color, chart, title, description }) {
-  return (
-    <Card>
-      <CardHeader variant="gradient" color={color}>
-        <Chart {...chart} />
-      </CardHeader>
-      <CardBody className="p-6">
-        <Typography variant="h6" color="blue-gray">
-          {title}
-        </Typography>
-      </CardBody>
-    </Card>
-  );
-}
+// export function StatisticsChart({ color, chart, title, description }) {
+//   return (
+//     <Card>
+//       <CardHeader variant="gradient" color={color}>
+//         <Chart {...chart} />
+//       </CardHeader>
+//       <CardBody className="p-6">
+//         <Typography variant="h6" color="blue-gray">
+//           {title}
+//         </Typography>
+//       </CardBody>
+//     </Card>
+//   );
+// }
 
-StatisticsChart.defaultProps = {
-  color: "blue",
-  footer: null,
-};
+// StatisticsChart.defaultProps = {
+//   color: "blue",
+//   footer: null,
+// };
 
-StatisticsChart.propTypes = {
-  color: PropTypes.oneOf([
-    "white",
-    "blue-gray",
-    "gray",
-    "brown",
-    "deep-orange",
-    "orange",
-    "amber",
-    "yellow",
-    "lime",
-    "light-green",
-    "green",
-    "teal",
-    "cyan",
-    "light-blue",
-    "blue",
-    "indigo",
-    "deep-purple",
-    "purple",
-    "pink",
-    "red",
-  ]),
-  chart: PropTypes.object.isRequired,
-  title: PropTypes.node.isRequired
-};
+// StatisticsChart.propTypes = {
+//   color: PropTypes.oneOf([
+//     "white",
+//     "blue-gray",
+//     "gray",
+//     "brown",
+//     "deep-orange",
+//     "orange",
+//     "amber",
+//     "yellow",
+//     "lime",
+//     "light-green",
+//     "green",
+//     "teal",
+//     "cyan",
+//     "light-blue",
+//     "blue",
+//     "indigo",
+//     "deep-purple",
+//     "purple",
+//     "pink",
+//     "red",
+//   ]),
+//   chart: PropTypes.object.isRequired,
+//   title: PropTypes.node.isRequired
+// };
 
-StatisticsChart.displayName = "/src/widgets/charts/statistics-chart.jsx";
+// StatisticsChart.displayName = "/src/widgets/charts/statistics-chart.jsx";
 
-export default StatisticsChart;
+// export default StatisticsChart;
